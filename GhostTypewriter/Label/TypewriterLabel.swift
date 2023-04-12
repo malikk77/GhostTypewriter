a6d5c361d03b6a4a58497a7202dfe0c759c7591b
HABIB UR REHMAN <50659344+malikk77@users.noreply.github.com>
2023-04-12 20:40:48 +0500

    -updated speed and fix the crash.
    - after increasing the speed of typing it began to crash so i fix the crash aswell.

--- GhostTypewriter/Label/TypewriterLabel.swift
+++ GhostTypewriter/Label/TypewriterLabel.swift
@@ -64,7 +64,7 @@ public enum AnimationStyle {
 public final class TypewriterLabel: UILabel {
     
     /// The interval (time gap) between each character being animated on screen.
-    public var typingTimeInterval: TimeInterval = 0.001
+    public var typingTimeInterval: TimeInterval = 0.01
     
     /// Boolean for if the label is animating or not.
     public private(set) var isAnimating: Bool = false
@@ -174,7 +174,7 @@ public final class TypewriterLabel: UILabel {
                 return
             }
                                                                                                               
-                guard let ind = attributedText.string.startIndex else {return}                                                                                              
+               guard let ind = attributedText.string.startIndex else {return}                                                                                              
             
             let characterIndex = attributedText.string.index(ind, offsetBy: self.currentCharacterOffset)
             self.updateCharacterPresentation(atIndex: characterIndex)
