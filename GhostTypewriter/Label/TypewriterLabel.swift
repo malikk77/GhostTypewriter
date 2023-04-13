10520a51fa64c6132ad5d69f7adde528d2a83777
HABIB UR REHMAN <50659344+malikk77@users.noreply.github.com>
2023-04-13 13:16:38 +0500

    -Updated Value

--- GhostTypewriter/Label/TypewriterLabel.swift
+++ GhostTypewriter/Label/TypewriterLabel.swift
@@ -176,7 +176,8 @@ public final class TypewriterLabel: UILabel {
                                                                                                                                                                                                        
             
 
-let characterIndex = attributedText.string.index(attributedText.string.startIndex ?? String.Index(encodedOffset: 0), offsetBy: self.currentCharacterOffset)                                                                                                              
+        let int = String.Index(utf16Offset: 0, in: "")
+            let characterIndex = attributedText.string.index(attributedText.string.startIndex ?? int, offsetBy: self.currentCharacterOffset)                                                                                                              
             self.updateCharacterPresentation(atIndex: characterIndex)
             
             self.iterateToNextCharacterOffset()
